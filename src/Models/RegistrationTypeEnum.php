4a2feb21b39121b25f434a6b4334f2f2509cbad9
Benoit Babin <bbabin@decizif.com>
2024-06-05 20:26:09 -0400

    Visuals

--- src/Models/RegistrationTypeEnum.php
+++ src/Models/RegistrationTypeEnum.php
@@ -7,16 +7,16 @@ enum RegistrationTypeEnum: int
     use \Kompo\Auth\Models\Traits\EnumKompo;
 
     case RT_OPEN_ALL = 1;
-    case RT_ONLY_QR_CODE = 5;
-    case RT_MEMBERS_ONLY = 10;
+    case RT_MEMBERS_ONLY = 5;
+    case RT_ONLY_QR_CODE = 10;
 
     public function label()
     {
         return match ($this)
         {
-            static::RT_OPEN_ALL => __('events-registrations-open-all'),
-            static::RT_ONLY_QR_CODE => __('events-registrations-qrcode'),
-            static::RT_MEMBERS_ONLY => __('events-registrations-members-onlys'),
+            static::RT_OPEN_ALL => __('events.registrations-open-all'),
+            static::RT_MEMBERS_ONLY => __('events.registrations-members-only'),
+            static::RT_ONLY_QR_CODE => __('events.registrations-qrcode'),
         };
     }
 }
