91f41458eaaf5ea81ec1b46038a951b77a96abde
Ezequiel <tavelliezequiel@gmail.com>
2024-08-27 16:33:39 -0300

    Fixes

--- src/Kompo/Inscriptions/InscriptionLandingPage.php
+++ src/Kompo/Inscriptions/InscriptionLandingPage.php
@@ -2,7 +2,6 @@
 
 namespace Condoedge\Crm\Kompo\Inscriptions;
 
-use App\Kompo\Inscriptions\InscriptionTypeEnum;
 use Kompo\Auth\Common\ImgFormLayout;
 
 class InscriptionLandingPage extends ImgFormLayout
