7a47bc5728625b2f466ce523069d9bb43f1f5ca5
Ezequiel <tavelliezequiel@gmail.com>
2025-01-10 11:17:58 -0300

    Nullable inscription

--- src/Models/Person.php
+++ src/Models/Person.php
@@ -4,218 +4,221 @@ namespace Condoedge\Crm\Models;
 
 use App\Models\User;
 use Condoedge\Crm\Facades\PersonModel;
-use Kompo\Auth\Models\Contracts\Searchable;
-use Kompo\Auth\Models\Model;
 use Kompo\Auth\Facades\RoleModel;
+use Kompo\Auth\Models\Contracts\Searchable;
 use Kompo\Auth\Models\Email\Email;
+use Kompo\Auth\Models\Model;
 
 abstract class Person extends Model implements Searchable
 {
-	use \Kompo\Auth\Models\Email\MorphManyEmails;
-	use \Kompo\Auth\Models\Maps\MorphManyAddresses;
-	use \Kompo\Auth\Models\Phone\MorphManyPhones;
-	use \Kompo\Auth\Models\Files\MorphManyFilesTrait;
+    use \Kompo\Auth\Models\Email\MorphManyEmails;
+    use \Kompo\Auth\Models\Maps\MorphManyAddresses;
+    use \Kompo\Auth\Models\Phone\MorphManyPhones;
+    use \Kompo\Auth\Models\Files\MorphManyFilesTrait;
 
-	use \Condoedge\Crm\Models\PersonInscriptionsRelatedTrait;
-	use \Condoedge\Crm\Models\PersonCharacteristicsRelatedTrait;
+    use \Condoedge\Crm\Models\PersonInscriptionsRelatedTrait;
+    use \Condoedge\Crm\Models\PersonCharacteristicsRelatedTrait;
 
-	protected $casts = [
-		'gender' => GenderEnum::class,
-		'spoken_languages' => 'array',
-	];
+    protected $casts = [
+        'gender' => GenderEnum::class,
+        'spoken_languages' => 'array',
+    ];
 
-	protected $table = 'persons';
+    protected $table = 'persons';
 
-	public function save(array $options = [])
-	{
-		if ($this->email_identity && $this->getDirty('email_identity') && ($email = $this->emails()->where('address_em', $this->getOriginal("email_identity"))->first())) {
-			$email->address_em = $this->email_identity;
-			$email->save();
-		}
+    public function save(array $options = [])
+    {
+        if ($this->email_identity && $this->getDirty('email_identity') && ($email = $this->emails()->where('address_em', $this->getOriginal("email_identity"))->first())) {
+            $email->address_em = $this->email_identity;
+            $email->save();
+        }
 
-		if ($this->exists && $this->email_identity && !$this->emails()->count()) {
-			Email::createMainFor($this, $this->email_identity);
-		}
+        if ($this->exists && $this->email_identity && !$this->emails()->count()) {
+            Email::createMainFor($this, $this->email_identity);
+        }
 
-		parent::save($options);
-	}
+        parent::save($options);
+    }
 
 
-	/* RELATIONS */
-	public function personEvents()
-	{
-		return $this->hasMany(PersonEvent::class);
-	}
+    /* RELATIONS */
+    public function personEvents()
+    {
+        return $this->hasMany(PersonEvent::class);
+    }
 
-	public function relatedUser()
-	{
-		return $this->belongsTo(User::class, 'user_id');
-	}
+    public function relatedUser()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 
-	public function person1Links()
-	{
-		return $this->hasMany(PersonLink::class, 'person1_id');
-	}
+    public function person1Links()
+    {
+        return $this->hasMany(PersonLink::class, 'person1_id');
+    }
 
-	public function person2Links()
-	{
-		return $this->hasMany(PersonLink::class, 'person2_id');
-	}
+    public function person2Links()
+    {
+        return $this->hasMany(PersonLink::class, 'person2_id');
+    }
 
-	public function personTeams()
-	{
-		return $this->hasMany(PersonTeam::class);
-	}
+    public function personTeams()
+    {
+        return $this->hasMany(PersonTeam::class);
+    }
 
-	public function diciplinaryActions()
-	{
-		return $this->hasMany(DiciplinaryAction::class);
-	}
+    public function diciplinaryActions()
+    {
+        return $this->hasMany(DiciplinaryAction::class);
+    }
 
-	/* SCOPES */
-	public function scopeActive($query, $teamId = null)
-	{
-		return $query->whereHas('personTeams', fn($q) => $q->whereNull('to')
-			->when($teamId, fn($q) => $q->where('team_id', $teamId))
-		);
-	}
+    /* SCOPES */
+    public function scopeActive($query, $teamId = null)
+    {
+        return $query->whereHas(
+            'personTeams',
+            fn ($q) => $q->whereNull('to')
+            ->when($teamId, fn ($q) => $q->where('team_id', $teamId))
+        );
+    }
 
     public function scopeAddFullName($query)
     {
-    	$query->selectRaw("id, CONCAT(first_name,' ',last_name) as person_full_name");
+        $query->selectRaw("id, CONCAT(first_name,' ',last_name) as person_full_name");
     }
 
-	public function scopeParent($query)
-	{
-		return $query->whereDoesntHave('person2Links');
-	}
+    public function scopeParent($query)
+    {
+        return $query->whereDoesntHave('person2Links');
+    }
 
-	public function scopeChild($query)
-	{
-		return $query->whereHas('person2Links');
-	}
+    public function scopeChild($query)
+    {
+        return $query->whereHas('person2Links');
+    }
 
-	public function scopeHasActiveTeam($query)
-	{
-		return $query->whereHas('personTeams', fn($q) => $q->active());
-	}
+    public function scopeHasActiveTeam($query)
+    {
+        return $query->whereHas('personTeams', fn ($q) => $q->active());
+    }
 
-	public function scopeOnlyInThisTeam($query, $teamId = null)
-	{
-		return $query->whereHas('personTeams', fn($q) => $q->where('team_id', $teamId ?? currentTeamId()));
-	}
+    public function scopeOnlyInThisTeam($query, $teamId = null)
+    {
+        return $query->whereHas('personTeams', fn ($q) => $q->where('team_id', $teamId ?? currentTeamId()));
+    }
 
-	public function scopeSearchByEmail($query, $email)
-	{
-		return $query->where(fn($q) => $q->where('email_identity', $email)
-			->orWhereHas('emails', fn($q) => $q->where('address_em', $email))
-		);
-	}
+    public function scopeSearchByEmail($query, $email)
+    {
+        return $query->where(
+            fn ($q) => $q->where('email_identity', $email)
+            ->orWhereHas('emails', fn ($q) => $q->where('address_em', $email))
+        );
+    }
 
-	/* CALCULATED FIELDS */
-	public function getAllPersonLinks()
-	{
-		return $this->person1Links()->with('person2')->get()->concat(
-			$this->person2Links()->with('person1')->get()
-		)->map(fn($pl) => $pl->setOtherAsPerson($this->id));
-	}
+    /* CALCULATED FIELDS */
+    public function getAllPersonLinks()
+    {
+        return $this->person1Links()->with('person2')->get()->concat(
+            $this->person2Links()->with('person1')->get()
+        )->map(fn ($pl) => $pl->setOtherAsPerson($this->id));
+    }
 
-	public function getFullNameAttribute()
-	{
-		return getFullName($this->first_name, $this->last_name);
-	}
+    public function getFullNameAttribute()
+    {
+        return getFullName($this->first_name, $this->last_name);
+    }
 
-	public function hasActiveBlock()
-	{
-		return $this->diciplinaryActions()->active()->blockType()->exists();
-	}
+    public function hasActiveBlock()
+    {
+        return $this->diciplinaryActions()->active()->blockType()->exists();
+    }
 
-	public function hasActiveBan()
-	{
-		return $this->diciplinaryActions()->active()->banType()->exists();
-	}
+    public function hasActiveBan()
+    {
+        return $this->diciplinaryActions()->active()->banType()->exists();
+    }
 
-	public function getActivityStatus()
-	{
-		if($this->hasActiveBan()) {
-			return 'crm.banned';
-		}
+    public function getActivityStatus()
+    {
+        if ($this->hasActiveBan()) {
+            return 'crm.banned';
+        }
 
-		if($this->hasActiveBlock()) {
-			return 'crm.blocked';
-		}
+        if ($this->hasActiveBlock()) {
+            return 'crm.blocked';
+        }
 
-		if (!$this->personTeams->count()) {
-			return 'crm.pending';
-		}
+        if (!$this->personTeams->count()) {
+            return 'crm.pending';
+        }
 
-		if ($this->personTeams->whereNull('to')->first()) {
-			return 'crm.active';
-		}
+        if ($this->personTeams->whereNull('to')->first()) {
+            return 'crm.active';
+        }
 
-		return 'crm.inactive';
-	}
+        return 'crm.inactive';
+    }
 
-	public static function getOptionsForTeamWithFullName($teamId)
-	{
-		return static::active($teamId)->addFullName()->pluck('person_full_name', 'id');
-	}
+    public static function getOptionsForTeamWithFullName($teamId)
+    {
+        return static::active($teamId)->addFullName()->pluck('person_full_name', 'id');
+    }
 
-	public function getRegisteringPerson()
-	{
-		return $this->registeredBy ?: $this;
-	}
+    public function getRegisteringPerson()
+    {
+        return $this->registeredBy ?: $this;
+    }
 
-	public function getRegisteringPersonEmail()
-	{
-		return $this->getRegisteringPerson()->email_identity;
-	}
+    public function getRegisteringPersonEmail()
+    {
+        return $this->getRegisteringPerson()->email_identity;
+    }
 
-	/* ACTIONS */
-	public static function retrieveByEmailIdentity($email)
-	{
-		$person = PersonModel::searchByEmail($email)->first();
+    /* ACTIONS */
+    public static function retrieveByEmailIdentity($email)
+    {
+        $person = PersonModel::searchByEmail($email)->first();
 
-		if (!$person) {
-			$person = PersonModel::newPersonFromEmail($email);
-		}
+        if (!$person) {
+            $person = PersonModel::newPersonFromEmail($email);
+        }
 
-		return $person;
-	}
+        return $person;
+    }
 
-	public function constructFakeEmail()
-	{
-		return \Str::slug($this->full_name) . '@user.coolecto.com';
-	}
+    public function constructFakeEmail()
+    {
+        return \Str::slug($this->full_name) . '@user.coolecto.com';
+    }
 
-	public function createOrGetUserByRegisteredBy($inscription, $team)
-	{
-		$user = User::where('email', $this->email ?: $this->constructFakeEmail())->first();
+    public function createOrGetUserByRegisteredBy($inscription, $team)
+    {
+        $user = User::where('email', $this->email ?: $this->constructFakeEmail())->first();
 
-		if(!$user) {
-			$user = User::create([
-				'name' => $this->full_name,
-				'email' => $this->constructFakeEmail(), // TODO we could do the email nullable 
-				'password' => bcrypt(value: \Str::random(12)),
-			]);
-		}
+        if (!$user) {
+            $user = User::create([
+                'name' => $this->full_name,
+                'email' => $this->constructFakeEmail(), // TODO we could do the email nullable
+                'password' => bcrypt(value: \Str::random(12)),
+            ]);
+        }
 
-		// Get the role for the children getting first the child inscription type
-		if ($role = $inscription->type->getRegisteredByRole()?->getRole($inscription)) {
-			RoleModel::getOrCreate($role);
-			
-			$teamRole = $user->createTeamRole($team, $role);
+        // Get the role for the children getting first the child inscription type
+        if ($role = $inscription->type->getRegisteredByRole()?->getRole($inscription)) {
+            RoleModel::getOrCreate($role);
 
-			PersonTeam::where('person_id', $this->id)->where('team_id', $team->id)->whereNull('team_role_id')
-				->update(['team_role_id' => $teamRole->id]);
-		}
-		
-	}
+            $teamRole = $user->createTeamRole($team, $role);
 
-	/* SEARCHABLE */
+            PersonTeam::where('person_id', $this->id)->where('team_id', $team->id)->whereNull('team_role_id')
+                ->update(['team_role_id' => $teamRole->id]);
+        }
+
+    }
+
+    /* SEARCHABLE */
     public function scopeSearch($query, $search)
     {
-    	$query->whereRaw("CONCAT(first_name,' ',last_name) LIKE ?", [ wildcardSpace($search)])
+        $query->whereRaw("CONCAT(first_name,' ',last_name) LIKE ?", [ wildcardSpace($search)])
             ->orWhereRaw("CONCAT(last_name,' ',first_name) LIKE ?", [ wildcardSpace($search)]);
     }
 
@@ -230,17 +233,17 @@ abstract class Person extends Model implements Searchable
         )->redirect('member.page', ['id' => $result->id]);
     }
 
-	/* ELEMENTS */
-	public function nameAndContactLabelsEl()
-	{
-		$phone = $this->getFirstValidPhoneLabel();
-		$address = $this->getFirstValidAddressLabel();
+    /* ELEMENTS */
+    public function nameAndContactLabelsEl()
+    {
+        $phone = $this->getFirstValidPhoneLabel();
+        $address = $this->getFirstValidAddressLabel();
 
-		return _Rows(
-			_LabelWithIcon('profile', $this->full_name),
-			!$phone ? null : _PhoneWithIcon($phone),
-			!$this->email_identity ? null : _EmailWithIcon($this->email_identity),
+        return _Rows(
+            _LabelWithIcon('profile', $this->full_name),
+            !$phone ? null : _PhoneWithIcon($phone),
+            !$this->email_identity ? null : _EmailWithIcon($this->email_identity),
             !$address ? null : _AddressWithIcon($address),
-		)->class('mb-3');
-	}
+        )->class('mb-3');
+    }
 }
--- src/Models/PersonEvent.php
+++ src/Models/PersonEvent.php
@@ -7,118 +7,118 @@ use Kompo\Auth\Models\Model;
 
 class PersonEvent extends Model
 {
-	use \Condoedge\Crm\Models\BelongsToPersonTrait;
-	use \Condoedge\Crm\Models\BelongsToEventTrait;
+    use \Condoedge\Crm\Models\BelongsToPersonTrait;
+    use \Condoedge\Crm\Models\BelongsToEventTrait;
 
-	protected $casts = [
-		'register_status' => RegisterStatusEnum::class,
-	];
+    protected $casts = [
+        'register_status' => RegisterStatusEnum::class,
+    ];
 
-	/* RELATIONS */
-	public function inscription()
-	{
-		return $this->belongsTo(InscriptionModel::getClass());
-	}
+    /* RELATIONS */
+    public function inscription()
+    {
+        return $this->belongsTo(InscriptionModel::getClass());
+    }
 
-	/* SCOPES */
-	public function scopeCountInTotal($query)
-	{
-		$query->whereIn('register_status', [
-			RegisterStatusEnum::RS_ACCEPTED,
-			RegisterStatusEnum::RS_PAID,
-		]);
-	}
+    /* SCOPES */
+    public function scopeCountInTotal($query)
+    {
+        $query->whereIn('register_status', [
+            RegisterStatusEnum::RS_ACCEPTED,
+            RegisterStatusEnum::RS_PAID,
+        ]);
+    }
 
-	public function scopeAwaitingApproval($query)
-	{
-		$query->whereIn('register_status', [
-			RegisterStatusEnum::RS_REQUESTED,
-		]);
-	}
+    public function scopeAwaitingApproval($query)
+    {
+        $query->whereIn('register_status', [
+            RegisterStatusEnum::RS_REQUESTED,
+        ]);
+    }
 
-	/* CALCULATED FIELDS */
-	public function getIeStatusLabelAttribute()
-	{
-		return $this->register_status->label();
-	}
+    /* CALCULATED FIELDS */
+    public function getIeStatusLabelAttribute()
+    {
+        return $this->register_status->label();
+    }
 
-	public function getRegisteringPerson()
-	{
-		return $this->person->registeredBy ?: $this->person;
-	}
+    public function getRegisteringPerson()
+    {
+        return $this->person->registeredBy ?: $this->person;
+    }
 
-	public function getRegisteringPersonEmail()
-	{
-		return $this->getRegisteringPerson()->email_identity;
-	}
+    public function getRegisteringPersonEmail()
+    {
+        return $this->getRegisteringPerson()->email_identity;
+    }
 
-	public function getRelatedTargetTeam()
-	{
-		return $this->event->team;
-	}
+    public function getRelatedTargetTeam()
+    {
+        return $this->event->team;
+    }
 
-	public function getFirstRegisteredPerson()
-	{
-		return $this->getRelatedRegistrations()->first();
-	}
+    public function getFirstRegisteredPerson()
+    {
+        return $this->getRelatedRegistrations()->first();
+    }
 
-	public function getNextRegisteredPerson()
-	{
-		return $this->getRelatedRegistrations()->where('id', '>', $this->id)->first();
-	}
+    public function getNextRegisteredPerson()
+    {
+        return $this->getRelatedRegistrations()->where('id', '>', $this->id)->first();
+    }
 
-	public function getRelatedRegistrations()
-	{
-		return static::where('inscription_id', $this->inscription_id)->get();
-	}
+    public function getRelatedRegistrations()
+    {
+        return static::where('inscription_id', $this->inscription_id)->get();
+    }
 
-	/* ROUTES */
-	public function getAcceptInscriptionUrl()
-	{
-		$inscription = $this->inscription ?: InscriptionModel::getOrCreateForPerson($this->person_id, $this->event->team_id, InscriptionModel::defaultTypeForPersonEvent());
-		
-		if (!$inscription->team_id) {
-			$inscription->team_id = $this->event->team_id;
-			$inscription->save();
-		}
+    /* ROUTES */
+    public function getAcceptInscriptionUrl()
+    {
+        $inscription = $this->inscription ?: InscriptionModel::getOrCreateForPerson($this->person_id, $this->event->team_id, InscriptionModel::defaultTypeForPersonEvent());
 
-		return $inscription->getAcceptInscriptionUrl();
-	}
+        if (!$inscription->team_id) {
+            $inscription->team_id = $this->event->team_id;
+            $inscription->save();
+        }
 
-	public function getInscriptionDoneRoute()
-	{
+        return $inscription->getAcceptInscriptionUrl();
+    }
+
+    public function getInscriptionDoneRoute()
+    {
         return \URL::signedRoute('inscription.done1', [
             'id' => $this->id,
         ]);
-	}
+    }
 
-	/* ACTIONS */
-	public static function createPersonEvent($person, $event, $inscription)
-	{
-		$pr = new static();
-		$pr->person_id = $person->id;
-		$pr->event_id = $event->id;
-		$pr->inscription_id = $inscription->id;
-		$pr->save();
+    /* ACTIONS */
+    public static function createPersonEvent($person, $event, $inscription)
+    {
+        $pr = new static();
+        $pr->person_id = $person->id;
+        $pr->event_id = $event->id;
+        $pr->inscription_id = $inscription?->id;
+        $pr->save();
 
-		return $pr;
-	}
+        return $pr;
+    }
 
-	public function approveAndSend()
-	{
-		$this->register_status = RegisterStatusEnum::RS_ACCEPTED;
-		$this->save();
+    public function approveAndSend()
+    {
+        $this->register_status = RegisterStatusEnum::RS_ACCEPTED;
+        $this->save();
 
-		$this->approveAndSendCustomCode();
+        $this->approveAndSendCustomCode();
 
-		// \Mail::to($this->getRegisteringPersonEmail())
+        // \Mail::to($this->getRegisteringPersonEmail())
         //     ->send(new \Condoedge\Crm\Mail\PersonInscriptionConfirmationMail($this->id));
-	}
+    }
 
-	protected function approveAndSendCustomCode()
-	{
-		//Override in App
-	}
+    protected function approveAndSendCustomCode()
+    {
+        //Override in App
+    }
 
-	/* ELEMENTS */
+    /* ELEMENTS */
 }
