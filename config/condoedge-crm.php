e97d6bf22045541d8a271aa084aafc9171682c95
Ezequiel <tavelliezequiel@gmail.com>
2025-01-22 07:31:57 -0300

    Minor fix config

--- config/condoedge-crm.php
+++ config/condoedge-crm.php
@@ -6,7 +6,7 @@ return [
     'event-model-namespace' => Condoedge\Crm\Models\Event::class,
     'person-model-namespace' => Condoedge\Crm\Models\Person::class,
 
-    'inscription-model-namespace' => getAppClass(App\Models\Inscriptions\Inscription::class, Condoedge\Crm\Inscriptions\Role::class),
+    'inscription-model-namespace' => getAppClass(App\Models\Inscriptions\Inscription::class, Condoedge\Crm\Models\Inscription::class),
 
     'inscription-types' => InscriptionTypeEnum::cases(),
 ];
