304b183b9a6e08284143d9c91af1bf7bf55a0aaa
Benoit Babin <bbabin@decizif.com>
2025-02-10 16:02:16 -0500

    center text

--- src/Kompo/Inscriptions/InscriptionLandingPage.php
+++ src/Kompo/Inscriptions/InscriptionLandingPage.php
@@ -44,7 +44,7 @@ class InscriptionLandingPage extends ImgFormLayout
 		return _Rows(
 			_LogoOnly()->class('self-center')->style('margin-bottom:60px;'),
             _RegistrableFromQrCodeTitle($this->qrCode),
-            _Html('inscriptions.i-want-to-register-and-i-am')->class('mb-6 text-lg'),
+            _Html('inscriptions.i-want-to-register-and-i-am')->class('mb-6 text-lg text-center'),
 
             _Rows(
                 collect(getInscriptionTypes())->filter(fn($it) => !$this->inscription?->type || $this->inscription?->type?->value == $it->value)
@@ -62,8 +62,8 @@ class InscriptionLandingPage extends ImgFormLayout
     protected function optionButton($type)
     {
         return _Rows(
-            _Html($type->registerTitle())->class('text-center !text-level1 font-semibold text-2xl mb-4'),
-            _Html($type->registerDescription())->class('!text-level1'),
+            _Html($type->registerTitle())->class('text-center !text-greendark font-semibold text-2xl mb-4'),
+            _Html($type->registerDescription())->class('!text-greendark'),
         )->button2()->class('rounded-lg !pt-3 p-6')->selfGet('manageInscriptionLink', ['type' => $type->value])->redirect();
     }
 }
