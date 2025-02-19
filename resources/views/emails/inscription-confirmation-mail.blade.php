79904853d00ac25b449d4fcd323b8ea2e4d7a1ed
Ezequiel <tavelliezequiel@gmail.com>
2025-02-19 19:33:04 -0300

    Minor fix email broken

--- resources/views/emails/inscription-confirmation-mail.blade.php
+++ resources/views/emails/inscription-confirmation-mail.blade.php
@@ -1,6 +1,6 @@
 @component('mail::message')
 
-<p>{{!! __('inscriptions.confirmation-email-message-1')}}</p>
+<p>{!! __('inscriptions.confirmation-email-message-1') !!}</p>
 
 <p>{!! makeMailButton(__('inscriptions.confirm-inscription'), $acceptInscriptionUrl) !!}</p>
 
