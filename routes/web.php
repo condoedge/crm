9597fdfbed3f44508b3da4a6f3b088dbdc20f825
Ezequiel <tavelliezequiel@gmail.com>
2025-06-12 18:47:10 -0300

    Registration -> trying to register as parent -> get permission error #550

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@
 
 use Condoedge\Crm\Http\Controllers\CustomInscriptionGenerable;
 
-Route::layout('layouts.guest')->middleware('guest')->group(function () {
+Route::layout('layouts.guest')->middleware('guest', 'disable-automatic-security')->group(function () {
 
     Route::get('join/{inscription_code?}', Condoedge\Crm\Kompo\Inscriptions\InscriptionLandingPage::class)->name('inscription.landing');
 
@@ -23,7 +23,7 @@ Route::layout('layouts.guest')->middleware('guest')->group(function () {
 
 });
 
-Route::middleware(['signed', 'throttle:10,1'])->group(function () {
+Route::middleware(['signed', 'throttle:10,1', 'disable-automatic-security'])->group(function () {
 
     Route::get('accept-inscription/{id}', Condoedge\Crm\Http\Controllers\PersonRegistrableAcceptController::class)->name('person-registrable.accept');
 
