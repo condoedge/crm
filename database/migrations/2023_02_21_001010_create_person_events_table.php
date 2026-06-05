047f0952a9fe65667f45e2f4f6b4a804ff7224b5
Ezequiel <tavelliezequiel@gmail.com>
2026-06-05 13:09:05 -0300

    Minor fix namespace

--- database/migrations/2023_02_21_001010_create_person_events_table.php
+++ database/migrations/2023_02_21_001010_create_person_events_table.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\Inscriptions\RegisterStatusEnum;
+use Condoedge\Crm\Models\RegisterStatusEnum;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
